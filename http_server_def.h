a9d24aca5e05c27c6b4dcba852d8b3ae48fa2790
AlbertoBSD <alberto.bsd@gmail.com>
2020-08-17 02:31:04 +0200

    POST data and map to C funtions in http_server_def.h

---
+++ http_server_def.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "http_headers.h"
+#include "debug.h"
+
+typedef char* (*TWrapper)(HTTP_HEADERS,HTTP_HEADERS,HTTP_HEADERS,HTTP_HEADERS,HTTP_HEADERS);
+/*
+	User defined funtions
+*/
+char *cfi_data(HTTP_HEADERS _SERVER,HTTP_HEADERS _GET,HTTP_HEADERS _POST,HTTP_HEADERS _HEADERS,HTTP_HEADERS _HEADERS_REPLY);
+/*
+	Backend map
+*/
+#define BACKEND_LENGTH 1
+
+char *urls[BACKEND_LENGTH] = {"/data"};
+TWrapper funtions[BACKEND_LENGTH] = {&cfi_data};
+
+char *cfi_data(HTTP_HEADERS _SERVER,HTTP_HEADERS _GET,HTTP_HEADERS _POST,HTTP_HEADERS _HEADERS,HTTP_HEADERS _HEADERS_REPLY)	{
+	char *buffer = NULL;
+	printf("Entrando a cfi_data\n");
+	if(http_headers_isset(_POST,"param1"))	{
+		printf("Si existe param1: %s\n",http_headers_get_value(_POST,"param1"));
+	}
+	return buffer;
+}
